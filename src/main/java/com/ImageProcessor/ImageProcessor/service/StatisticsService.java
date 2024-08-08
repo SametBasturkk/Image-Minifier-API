23c1bde27acee45cc21c006c1eac96253122498b
Samet <sametklou1907@gmail.com>
2024-08-08 23:47:31 +0300

    lock mechanism added

--- src/main/java/com/ImageProcessor/ImageProcessor/service/StatisticsService.java
+++ src/main/java/com/ImageProcessor/ImageProcessor/service/StatisticsService.java
@@ -2,35 +2,39 @@ package com.ImageProcessor.ImageProcessor.service;
 
 import com.ImageProcessor.ImageProcessor.model.Statistics;
 import com.ImageProcessor.ImageProcessor.repository.StatisticsRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-@Slf4j
 @Service
 public class StatisticsService {
 
-    private StatisticsRepository statisticsRepository;
+    private final StatisticsRepository statisticsRepository;
+    private final Lock lock = new ReentrantLock();
 
     public StatisticsService(StatisticsRepository statisticsRepository) {
         this.statisticsRepository = statisticsRepository;
     }
 
     public void updateStatistic(Long compressedSize, Long originalSize) {
-        Optional<Statistics> statistics = statisticsRepository.findById(1);
+        lock.lock();
+        try {
+            Optional<Statistics> statistics = statisticsRepository.findById(1);
 
-        if (statistics.isPresent()) {
-            Statistics stats = statistics.get();
-            stats.setTotalImagesProcessed(stats.getTotalImagesProcessed() + 1);
-            stats.setTotalBytesProcessed(stats.getTotalBytesProcessed() + originalSize);
-            stats.setTotalBytesSaved(stats.getTotalBytesSaved() + (originalSize - compressedSize));
-            statisticsRepository.save(stats);
-        } else {
-            Statistics stats = new Statistics(1, originalSize - compressedSize, originalSize);
-            statisticsRepository.save(stats);
+            if (statistics.isPresent()) {
+                Statistics stats = statistics.get();
+                stats.setTotalImagesProcessed(stats.getTotalImagesProcessed() + 1);
+                stats.setTotalBytesProcessed(stats.getTotalBytesProcessed() + originalSize);
+                stats.setTotalBytesSaved(stats.getTotalBytesSaved() + (originalSize - compressedSize));
+                statisticsRepository.save(stats);
+            } else {
+                Statistics stats = new Statistics(1, originalSize - compressedSize, originalSize);
+                statisticsRepository.save(stats);
+            }
+        } finally {
+            lock.unlock();
         }
     }
-
-
 }
