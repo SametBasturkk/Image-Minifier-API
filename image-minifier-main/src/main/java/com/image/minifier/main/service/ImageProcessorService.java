35c37952db980111f83598d0536c57aa431dead3
Samet <sametklou1907@gmail.com>
2024-08-17 18:43:39 +0300

    todo handle supported extension at the network layer

--- image-minifier-main/src/main/java/com/image/minifier/main/service/ImageProcessorService.java
+++ image-minifier-main/src/main/java/com/image/minifier/main/service/ImageProcessorService.java
@@ -2,27 +2,22 @@ package com.image.minifier.main.service;
 
 import com.image.minifier.main.dto.CompressedImageResponse;
 import com.image.minifier.main.exception.FileProcessingException;
-import com.image.minifier.main.exception.UnsupportedFileTypeException;
 import com.image.minifier.main.model.ImageStatus;
 import com.image.minifier.main.producer.KafkaPublisherService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.*;
 
 @Slf4j
 @Service
 public class ImageProcessorService {
-
-    @Value("${app.supported.extensions}")
-    private String[] supportedExtensions;
 
     private final KafkaPublisherService kafkaPublisherService;
     private final StatisticsService statisticsService;
@@ -37,11 +32,9 @@ public class ImageProcessorService {
     }
 
     public ResponseEntity<CompressedImageResponse> processImage(MultipartFile file, Integer quality) {
-        validateFile(file);
-        String extension = getExtension(file);
-
+        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename().toLowerCase());
         try {
-            UUID compressedFilePathUUID = kafkaPublisherService.publishCompressImageTopic(file, quality, "." + extension.toLowerCase());
+            UUID compressedFilePathUUID = kafkaPublisherService.publishCompressImageTopic(file, quality, "." + extension);
             ImageStatus imageStatus = new ImageStatus(compressedFilePathUUID, false, null);
             imageStatusService.saveImageStatus(imageStatus);
             log.info("Image processing started for file: {}", file.getOriginalFilename());
@@ -53,27 +46,6 @@ public class ImageProcessorService {
         }
     }
 
-    private void validateFile(MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            throw new IllegalArgumentException("File must not be empty");
-        }
-        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
-        if (extension == null || !isSupportedExtension(extension)) {
-            throw new UnsupportedFileTypeException("Unsupported or unrecognized file type");
-        }
-    }
-
-    private String getExtension(MultipartFile file) {
-        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
-        if (extension == null) {
-            throw new UnsupportedFileTypeException("File type could not be determined");
-        }
-        return extension;
-    }
-
-    private boolean isSupportedExtension(String extension) {
-        return Arrays.stream(supportedExtensions).anyMatch(extension::equalsIgnoreCase);
-    }
 
     private ResponseEntity<CompressedImageResponse> waitForImageCompression(ImageStatus imageStatus, MultipartFile file) {
         CompletableFuture<ResponseEntity<CompressedImageResponse>> responseFuture = new CompletableFuture<>();
@@ -103,24 +75,15 @@ public class ImageProcessorService {
         byte[] compressedImageData = status.getCompressedBase64Data();
         long originalSize = file.getSize();
         long compressedSize = compressedImageData.length;
-        double compressionRatio = calculateCompressionRatio(originalSize, compressedSize);
+        double compressionRatio = ((double) (originalSize - compressedSize) / originalSize) * 100;
 
         log.info("Original size: {}, Compressed size: {}, Compression ratio: {}%", originalSize, compressedSize, compressionRatio);
 
         CompressedImageResponse response = new CompressedImageResponse(compressedImageData, file.getOriginalFilename(), originalSize, compressedSize, compressionRatio);
         statisticsService.updateCounterStatistic(compressedSize, originalSize);
 
-        responseFuture.complete(ResponseEntity.ok().headers(createHeaders(file)).contentType(MediaType.APPLICATION_JSON).body(response));
+        responseFuture.complete(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response));
     }
 
-    private double calculateCompressionRatio(long originalSize, long compressedSize) {
-        return ((double) (originalSize - compressedSize) / originalSize) * 100 ;
-    }
 
-    private HttpHeaders createHeaders(MultipartFile file) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDisposition(ContentDisposition.attachment().filename(file.getOriginalFilename()).build());
-        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-        return headers;
-    }
 }
