d2a82a6241279fd9b28a6588ae33d9eca2997729
Samet <sametklou1907@gmail.com>
2024-08-23 10:24:32 +0300

    test

--- image-minifier-main/src/main/java/com/image/minifier/main/service/UserService.java
+++ image-minifier-main/src/main/java/com/image/minifier/main/service/UserService.java
@@ -2,6 +2,9 @@ package com.image.minifier.main.service;
 
 import com.image.minifier.main.configuration.KeycloakConfig;
 import com.image.minifier.main.dto.CreateUserRequest;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserService {
 
     private KeycloakConfig keycloak;
@@ -32,11 +36,16 @@ public class UserService {
         user.setLastName(request.getLastName());
         user.setEnabled(true);
         user.setEmailVerified(true);
-        keycloak.run().realm(REALM).users().create(user);
+        UsersResource userResource = keycloak.run().realm(REALM).users();
+        Response response = userResource.create(user);
+        log.info("User {} created", request.getUsername());
+        log.info("Response status: {}", response.getStatus());
+        response.close();
     }
 
     public void deleteUser(String username) {
         keycloak.run().realm(REALM).users().delete(username);
+        log.info("User {} deleted", username);
     }
 
     public void updateUser(String username, CreateUserRequest request) {
@@ -45,10 +54,13 @@ public class UserService {
         user.setEmail(request.getEmail());
         user.setCredentials(createCredentialRepresentation(request.getPassword()));
         keycloak.run().realm(REALM).users().get(user.getId()).update(user);
+        log.info("User {} updated", username);
     }
 
     public List<UserRepresentation> getUsers() {
-        return keycloak.run().realm(REALM).users().list();
+        List<UserRepresentation> listOfUsers = keycloak.run().realm(REALM).users().list();
+        log.info("List of users: {}", listOfUsers);
+        return listOfUsers;
     }
 
 
@@ -59,6 +71,7 @@ public class UserService {
         passwordCred.setType(CredentialRepresentation.PASSWORD);
         passwordCred.setValue(password);
         resp.add(passwordCred);
+        log.info("Credential representation created");
         return resp;
     }
 
