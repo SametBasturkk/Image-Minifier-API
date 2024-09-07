695af18390dccd9a9203b0bd9208d6c5d2ffafab
Samet <sametklou1907@gmail.com>
2024-09-07 14:01:36 +0300

    u

--- image-minifier-main/src/main/java/com/image/minifier/main/configuration/KafkaProducerConfig.java
+++ image-minifier-main/src/main/java/com/image/minifier/main/configuration/KafkaProducerConfig.java
@@ -27,6 +27,7 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 10485760);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
