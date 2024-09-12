6acfce776c86902ef01b9bd758c60e5f6e5a9ab1
Samet <sametklou1907@gmail.com>
2024-09-12 22:09:52 +0300

    u

---
+++ image-minifier-common/src/test/java/com/image/minifier/common/util/FileUtilTest.java
@@ -0,0 +1,14 @@
+package com.image.minifier.common.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilTest {
+
+    @org.junit.jupiter.api.Test
+    void saveUploadedFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createCompressedDirectory() {
+    }
+}
---
+++ image-minifier-common/src/test/java/com/image/minifier/common/util/ObjectConverterTest.java
@@ -0,0 +1,14 @@
+package com.image.minifier.common.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ObjectConverterTest {
+
+    @org.junit.jupiter.api.Test
+    void mapToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stringToMap() {
+    }
+}
